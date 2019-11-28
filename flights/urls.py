5aea7763f0fec6b5301d54637d699c003734d75c
Jyothis Thomas <jyothisth@gmail.com>
2019-11-28 14:38:09 +0530

    Flight drf

---
+++ flights/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path, include
+from . import views
+from rest_framework import routers
+from django.conf.urls import url
+
+urlpatterns = [   
+    url(r'^(?P<pk>[0-9]+)$', views.flight_detail),
+    url(r'^$', views.flight_list)
+]
