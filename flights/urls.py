1fb9332104d2304608bc730602902789250497c2
Jyothis Thomas <jyothisth@gmail.com>
2019-11-29 17:46:02 +0530

    fixed regular expression

--- flights/urls.py
+++ flights/urls.py
@@ -7,6 +7,6 @@ from django.conf.urls import url
 urlpatterns = [   
     # url(r'^$', views.HomePageView.as_view()),
 
-    url(r'^(?P<pk>[0-9]*)$', views.flight_detail),
+    url(r'^(?P<pk>[0-9]+)$', views.flight_detail),
     url(r'^$', views.flight_list)
 ]
